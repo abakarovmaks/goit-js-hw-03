65623c21259abe8ad787ff125828f5e1e7c16fc4
abakarovmaks <abakarovmaks@gmail.com>
2020-08-27 11:59:01 +0300

    Решает задачу 5

--- task-5.js
+++ task-5.js
@@ -1,14 +1,23 @@
-const checkForSpam = function (message) {
-  return message.toLowerCase().includes("spam") ||
-    message.toLowerCase().includes("sale")
-    ? true
-    : false;
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  let result = [];
+  for (const product of arr) {
+    if (product[prop] !== undefined) result.push(product[prop]);
+  }
+  return result;
 };
 
-console.log(checkForSpam("Latest technology news")); // false
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
 
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
 
-console.log(checkForSpam("Get best sale offers now!")); // true
-
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(getAllPropValues(products, "category")); // []
